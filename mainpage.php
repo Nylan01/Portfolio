734c1cb1e8dfc2845aa53033d58de88b77a7d60d
Nylan01 <nylanpaillassa01@gmail.com>
2024-08-30 14:09:02 +0200

    First commit

---
+++ mainpage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="author" content="Nylan Paillassa" />
+        <meta name="keywords" content="Portfolio" />
+        <title>Nylan Portfolio</title>
+        <link rel="stylesheet" type="text/css" href="mainpage.css" />
+        <script type="text/javascript" src="mainpage.js"></script>
+    </head>
+    <body>
+        <p>This is a paragraph.</p>
+        <p>This is another paragraph.</p>
+    </body>
+</html>
