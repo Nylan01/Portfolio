99b050a85e5838d2680bfb406e62330955ed19ad
Nylan01 <nylanpaillassa01@gmail.com>
2024-08-30 14:43:55 +0200

    test

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require 'vendor/autoload.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta name="author" content="Nylan Paillassa" />
+        <meta name="keywords" content="Portfolio" />
+        <title>Nylan Portfolio</title>
+        <link rel="stylesheet" type="text/css" href="mainpage.css" />
+        <script type="text/javascript" src="mainpage.js"></script>
+    </head>
+    <body>
+        <p>This is a paragraph.</p>
+        <p>This is another paragraph.</p>
+    </body>
+</html>
