32b6732fd60f1299535a096631566f253e44d0ef
Nylan01 <nylanpaillassa01@gmail.com>
2024-09-09 17:32:33 +0200

    color and font

--- index.php
+++ index.php
@@ -7,12 +7,15 @@ require 'vendor/autoload.php';
     <head>
         <meta name="author" content="Nylan Paillassa" />
         <meta name="keywords" content="Portfolio" />
+        <meta charset="utf-8" />
         <title>Nylan Portfolio</title>
         <link rel="stylesheet" type="text/css" href="mainpage.css" />
         <script type="text/javascript" src="mainpage.js"></script>
     </head>
     <body>
-        <p>This is a paragraph.</p>
-        <p>This is another paragraph.</p>
+        <div class=centered>
+            <p>This is a paragraph.</p>
+            <p>This is another paragraph.</p>   
+        </div>
     </body>
 </html>
