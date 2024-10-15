82f67c57e26fc80e9c5ba5d44a4333eb366266e9
Nylan01 <nylanpaillassa01@gmail.com>
2024-10-15 17:40:36 +0200

    size of the sections management

--- index.php
+++ index.php
@@ -12,20 +12,39 @@ require 'vendor/autoload.php';
         <link rel="stylesheet" type="text/css" href="mainpage.css" />
     </head>
     <body>
+        <div class='container'>
+            <section>
+                <h1 class='one'>
+                    Page d'entrée avec l'hologramme
+                </h1>
+            </section>
+            <section>
+                <h1 class='two'>
+                    Page de présentation sur moi et mon travail
+                </h1>
+            </section>
+            <section>
+                <h1 class='three'>
+                    Projets implémentés jeux etc...
+                </h1>
+            </section>
+        </div>
         <div id='preloader'>
             <p id='loadingmsg'>In the digital realm, the outsider sees what others miss. 
                 <br> Free from convention, they shape the future, coding from the shadows. 
                 <br>Embrace the unknown, and let your work disrupt the ordinary.
             </p>
         </div>
-        <p id='loadingmsg'>Ok</p>
-        <button type="button" onclick="location.href = 'test.php';">OK</button>
-        <div class=centered>
+
+        <!-- <div class=centered>
+            <p id='loadingmsg'>Ok</p>
+            <button type="button" onclick="location.href = 'test.php';">OK</button>
+        </div>
     <script>
         var loader = document.getElementById("preloader");
         window.addEventListener("load", function(){
             loader.style.display = "none";
-        });
+        }); -->
     </script>
     <script type="text/javascript" src="mainpage.js"></script>
     </body>
