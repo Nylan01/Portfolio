df9cb24d45358ff8f5f7ebaa2d897c76c8adc41c
Nylan01 <nylanpaillassa01@gmail.com>
2024-11-22 16:41:11 +0100

    ok

--- mainpage.php
+++ mainpage.php
@@ -1,18 +1,32 @@
-<?php
-require 'vendor/autoload.php';
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
     <head>
         <meta name="author" content="Nylan Paillassa" />
         <meta name="keywords" content="Portfolio" />
+        <meta charset="utf-8" />
         <title>Nylan Portfolio</title>
         <link rel="stylesheet" type="text/css" href="mainpage.css" />
-        <script type="text/javascript" src="mainpage.js"></script>
     </head>
     <body>
-        <p>This is a paragraph.</p>
-        <p>This is another paragraph.</p>
+        <!-- Logo en haut à droite de la page -->
+        <div class='logo'>
+            <img src='./imgs/28156689_christmas_2012_new_4027.svg' alt='Logo'>
+        </div>
+        <div >
+            <a href='./test/test.html' >Aller vers la page de test</a>
+        </div>
+        <div class='container'>
+            <section class='section one'>
+                <h1>Page d'entrée avec l'hologramme</h1>
+            </section>
+            <section class='section two'>
+                <h1>Page de présentation sur moi et mon travail</h1>
+            </section>
+            <section class='section three'>
+                <h1>Projets implémentés jeux etc...</h1>
+            </section>
+        </div>
+
+        <script type="text/javascript" src="mainpage.js"></script>
     </body>
 </html>
